a65d943c1e420e9fe9bc6dd49f8c10656ef9f982
arcfide <arcfide@sacrideo.us>
2009-12-22 04:52:14 +0000

    Make the c code compile on Windows and get rid of some things that we don't need.

--- sockets/socket-ffi-values.c
+++ sockets/socket-ffi-values.c
@@ -16,6 +16,24 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
+#ifdef __NT__
+#define WIN32
+#define EXPORTED __declspec ( dllexport ) int cdecl
+#endif
+#ifdef __WINDOWS__
+#define WIN32
+#define EXPORTED __declspec ( dllexport ) int cdecl
+#endif
+
+#ifndef WIN32
+#define EXPORTED int
+#endif
+
+#ifdef WIN32
+#include <stddef.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#else
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
@@ -24,10 +42,13 @@
 #include <errno.h>
 #include <sys/unistd.h>
 #include <sys/fcntl.h>
+#endif
 
-int get_ffi_value(const char *val) {
+EXPORTED get_ffi_value(const char *val) {
   struct sockaddr_in sai;
+#ifndef WIN32
   struct sockaddr_un sau;
+#endif
 
   if (!strcmp(val, "$ipproto-ip")) return IPPROTO_IP;
   if (!strcmp(val, "$ipproto-raw")) return IPPROTO_RAW;
@@ -38,17 +59,12 @@ int get_ffi_value(const char *val) {
   if (!strcmp(val, "%ai/numerichost")) return AI_NUMERICHOST;
   if (!strcmp(val, "%ai/passive")) return AI_PASSIVE;
   if (!strcmp(val, "%msg/dont-route")) return MSG_DONTROUTE;
-  if (!strcmp(val, "%msg/dont-wait")) return MSG_DONTWAIT;
+  /* if (!strcmp(val, "%msg/dont-wait")) return MSG_DONTWAIT; 
+  if (!strcmp(val, "%msg/wait-all")) return MSG_WAITALL; */
   if (!strcmp(val, "%msg/out-of-band")) return MSG_OOB;
   if (!strcmp(val, "%msg/peek")) return MSG_PEEK;
-  if (!strcmp(val, "%msg/wait-all")) return MSG_WAITALL;
-  if (!strcmp(val, "%shutdown/read")) return SHUT_WR;
-  if (!strcmp(val, "%shutdown/read&write")) return SHUT_RDWR;
-  if (!strcmp(val, "%shutdown/write")) return SHUT_RD;
   if (!strcmp(val, "%socket-domain/internet")) return AF_INET;
   if (!strcmp(val, "%socket-domain/internet-v6")) return AF_INET6;
-  if (!strcmp(val, "%socket-domain/local")) return AF_LOCAL;
-  if (!strcmp(val, "%socket-domain/unix")) return AF_UNIX;
   if (!strcmp(val, "%socket-type/datagram")) return SOCK_DGRAM;
   if (!strcmp(val, "%socket-type/random")) return SOCK_RDM;
   if (!strcmp(val, "%socket-type/raw")) return SOCK_RAW;
@@ -56,29 +72,48 @@ int get_ffi_value(const char *val) {
   if (!strcmp(val, "%socket-type/stream")) return SOCK_STREAM;
   if (!strcmp(val, "%somaxconn")) return SOMAXCONN;
   if (!strcmp(val, "af-inet")) return AF_INET;
-  if (!strcmp(val, "af-unix")) return AF_UNIX;
-  if (!strcmp(val, "invalid-socket")) return -1;
   if (!strcmp(val, "size-of/addr-in")) return sizeof(struct sockaddr_in);
-  if (!strcmp(val, "size-of/addr-un")) return sizeof(struct sockaddr_un);
   if (!strcmp(val, "size-of/addrinfo")) return sizeof(struct addrinfo);
   if (!strcmp(val, "size-of/integer")) return sizeof(int);
   if (!strcmp(val, "size-of/ip")) return sizeof(struct in_addr);
   if (!strcmp(val, "size-of/pointer")) return sizeof(void *);
   if (!strcmp(val, "size-of/port")) return sizeof(sai.sin_port); 
   if (!strcmp(val, "size-of/protoent")) return sizeof(struct protoent);
-  if (!strcmp(val, "size-of/sa-family")) return sizeof(sa_family_t);
   if (!strcmp(val, "size-of/size-t")) return sizeof(size_t);
   if (!strcmp(val, "size-of/sockaddr-in")) return sizeof(struct sockaddr_in);
-  if (!strcmp(val, "size-of/sockaddr-un")) return sizeof(struct sockaddr_un);
   if (!strcmp(val, "size-of/socklen-t")) return sizeof(socklen_t);
-  if (!strcmp(val, "unix-max-path")) return sizeof(sau.sun_path);
-  if (!strcmp(val, "$file-get-flag")) return F_GETFL;
+  /* if (!strcmp(val, "$file-get-flag")) return F_GETFL; */
+#ifdef WIN32
+  if (!strcmp(val, "size-of/sa-family")) return sizeof(unsigned short);
+  if (!strcmp(val, "%shutdown/read")) return SD_RECEIVE;
+  if (!strcmp(val, "%shutdown/read&write")) return SD_BOTH;
+  if (!strcmp(val, "%shutdown/write")) return SD_SEND;
+  if (!strcmp(val, "invalid-socket")) return INVALID_SOCKET;
+  if (!strcmp(val, "$file-set-flag")) return FIONBIO;
+  if (!strcmp(val, "$option-non-blocking")) return 1;
+  if (!strcmp(val, "$error-again")) return WSAEWOULDBLOCK;
+  if (!strcmp(val, "$error-in-progress")) return WSAEINPROGRESS;
+  if (!strcmp(val, "$error-would-block")) return WSAEWOULDBLOCK;
+  if (!strcmp(val, "$socket-error")) return SOCKET_ERROR;
+#else
+  if (!strcmp(val, "size-of/sa-family")) return sizeof(sa_family_t);
+  if (!strcmp(val, "%shutdown/read")) return SHUT_RD;
+  if (!strcmp(val, "%shutdown/read&write")) return SHUT_RDWR;
+  if (!strcmp(val, "%shutdown/write")) return SHUT_WR;
+  if (!strcmp(val, "invalid-socket")) return -1;
   if (!strcmp(val, "$file-set-flag")) return F_SETFL;
   if (!strcmp(val, "$option-non-blocking")) return O_NONBLOCK;
   if (!strcmp(val, "$error-again")) return EAGAIN;
   if (!strcmp(val, "$error-in-progress")) return EINPROGRESS;
   if (!strcmp(val, "$error-would-block")) return EWOULDBLOCK;
   if (!strcmp(val, "$socket-error")) return -1;
-  
+  if (!strcmp(val, "%socket-domain/local")) return AF_LOCAL;
+  if (!strcmp(val, "%socket-domain/unix")) return AF_UNIX;
+  if (!strcmp(val, "af-unix")) return AF_UNIX;
+  if (!strcmp(val, "size-of/sockaddr-un")) return sizeof(struct sockaddr_un);
+  if (!strcmp(val, "unix-max-path")) return sizeof(sau.sun_path);
+  if (!strcmp(val, "size-of/addr-un")) return sizeof(struct sockaddr_un);
+#endif
+
   return 0;
 }
